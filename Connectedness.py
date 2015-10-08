064fd5112370ce07c404fb24465e56255c54f6a0
Kristian Thorup <thorup.dk@gmail.com>
2015-10-08 15:09:01 +0200

    init

--- Connectedness.py
+++ Connectedness.py
@@ -37,6 +37,7 @@ def EstimateVAR(data, H):
 
     return pd.DataFrame(GVD), SIGMA, ma_rep, results.resid.T
 
+ # test
 
 def EstimateVAR_slow():
     df = pd.read_csv('C:/Users/thoru_000/Dropbox/Pers/PyCharmProjects/Speciale/data.csv', sep=";")
